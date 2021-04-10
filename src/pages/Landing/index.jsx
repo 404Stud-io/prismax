21feff206d30b9fff4cd8f272f226e1c227564e7
Alfredo Martinez Garcia <psialfredomtz@gmail.com>
2021-04-09 20:05:25 -0500

    [Update] response

--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 // Material
 import Form from '../../components/Form'
-import FormMailChimp from "../../components/FormMailChimp";
+// import FormMailChimp from "../../components/FormMailChimp";
 
 
 export default function Landing() {
 
   return (
     <>
-      {/* <Form/> */}
-      <FormMailChimp/>
+      <Form/>
+      {/* <FormMailChimp/> */}
     </>
   );
 }
