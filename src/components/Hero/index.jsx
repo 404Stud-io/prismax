c9c9cd9d449e2cd51e19c44343302b462252c368
Alfredo Martinez Garcia <psialfredomtz@gmail.com>
2021-04-20 00:16:05 -0500

    [Fix] route Form

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -4,17 +4,15 @@ import { Link } from "react-router-dom";
 import "./index.css";
 
 import Button from "@material-ui/core/Button";
-// import hero from '../../assets/hero.jpeg';
 
 export default function Hero() {
   return (
     <>
       <div className="imageContainer">
-        {/* <img src={hero} alt=""/> */}
         <div className="containerAction">
-          <Link to="/form">
+          <a href="https://registro.prismax.com.mx/">
             <Button variant="contained" className="buttonRegister">Registro</Button>
-          </Link>
+          </a>
           <Button variant="contained" className="buttonAccess">Acceso</Button>
         </div>
       </div>
