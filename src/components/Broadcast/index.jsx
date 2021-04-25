1762552d1a6f15649a77c5d79a8ca80374805f3b
Alfredo Martinez Garcia <psialfredomtz@gmail.com>
2021-04-25 11:47:11 -0500

    [Update] Broadcast component

---
+++ src/components/Broadcast/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './index.css'
+
+export default function Broadcast() {
+  return (
+    <section className="broadcastContainer">
+      
+    </section>
+  )
+}
--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Broadcast from "../../components/Broadcast";
 import Downloadable from "../../components/Downloadable";
 // Components
 import Hero from "../../components/Hero";
@@ -16,6 +17,7 @@ export default function Landing() {
       <Menu/>
       <Model3d/>
       <Downloadable/>
+      <Broadcast/>
     </>
   );
 }
