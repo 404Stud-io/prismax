965a28883b53a2e19a3c21c1514f47b00b1166c6
Alfredo Martinez Garcia <psialfredomtz@gmail.com>
2021-05-12 23:26:02 -0500

    [Fix] interlane

--- src/components/Downloadable/index.jsx
+++ src/components/Downloadable/index.jsx
@@ -2,19 +2,34 @@ import React from "react";
 // Style
 import "./index.css";
 // image
-import Agenda from '../../assets/agendaExample.png'
+import Agenda from "../../assets/agendaExample.png";
 export default function Downloadable() {
   return (
     <div className="downloaderContainer" id="download">
       <h1 className="title">Descargables</h1>
       <div className="containerMenu">
-        <a target="_blank"  rel="noreferrer" href="https://firebasestorage.googleapis.com/v0/b/mystorage-af4f8.appspot.com/o/BXT-557H_PrisMax%20SpecSheet_A4_PRINT-14DEC18_NoBleed_ESP%20(002).pdf?alt=media&token=38ff5b77-dec1-4f9e-9f74-69e5dacee22e" className="containerItemMenu prismax">
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://firebasestorage.googleapis.com/v0/b/mystorage-af4f8.appspot.com/o/BXT-557H_PrisMax%20SpecSheet_A4_PRINT-14DEC18_NoBleed_ESP%20(002).pdf?alt=media&token=38ff5b77-dec1-4f9e-9f74-69e5dacee22e"
+          className="containerItemMenu prismax"
+        >
           <p>Características PrisMax</p>
         </a>
-        <a target="_blank"  rel="noreferrer" href="https://firebasestorage.googleapis.com/v0/b/mystorage-af4f8.appspot.com/o/Catalogo%20Prismax%209%20de%20Marzo.pdf?alt=media&token=29a6c3b7-94fa-4b0e-9999-a1d1361662e6" className="containerItemMenu download">
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://firebasestorage.googleapis.com/v0/b/mystorage-af4f8.appspot.com/o/Catalogo%20Prismax%209%20de%20Marzo.pdf?alt=media&token=29a6c3b7-94fa-4b0e-9999-a1d1361662e6"
+          className="containerItemMenu download"
+        >
           <p>PrisMax</p>
         </a>
-        <a target="_blank"  rel="noreferrer" href="https://firebasestorage.googleapis.com/v0/b/mystorage-af4f8.appspot.com/o/PrisMax_Auto-Effluent-One-Pager%20GLBL_MG207_18-0009a(2)_ESP__V1.pdf?alt=media&token=f0622da0-ed2a-43f2-839f-3540a18afc7d" className="containerItemMenu schedule">
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://firebasestorage.googleapis.com/v0/b/mystorage-af4f8.appspot.com/o/PrisMax_Auto-Effluent-One-Pager%20GLBL_MG207_18-0009a(2)_ESP__V1.pdf?alt=media&token=f0622da0-ed2a-43f2-839f-3540a18afc7d"
+          className="containerItemMenu schedule"
+        >
           <p>Drenaje Autosuficiente</p>
         </a>
         {/* <div className="containerItemMenu transmission">
@@ -27,10 +42,10 @@ export default function Downloadable() {
 
       <div className="scheduleContainer " id="schedule">
         <h1> Agenda</h1>
-        <div className="dateChip"> 
-        <h2>13 de Mayo 2021</h2> 
-          </div>
-        <img src={Agenda} alt="Agenda"/>
+        <div className="dateChip">
+          <h2>13 de Mayo 2021</h2>
+        </div>
+        <img src={Agenda} alt="Agenda" />
       </div>
     </div>
   );
